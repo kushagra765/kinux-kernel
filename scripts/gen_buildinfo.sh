43bd3157df6bd338f1dc0f5ee9ddceb143b47c17
kushagra <kushagra765@outlook.com>
2022-05-10 18:35:44 +0530

    Fix build on Manjaro Linux
    When building the kernel on Manjaro, it fails with unterminated #ifndef
    error. Fix this by changing echo to printf in scripts/gen_config.sh .
    
    Signed-off-by: kushagra <kushagra765@outlook.com>

--- scripts/gen_buildinfo.sh
+++ scripts/gen_buildinfo.sh
@@ -5,4 +5,4 @@ GCCVERSION=`./toolchain/compiler/bin/i686-elf-gcc --version | grep ^i686-elf-gcc
 
 mkdir -p include/generated/
 
-echo "#ifndef _KINUX_BUILD_INFO_H\n#define _KINUX_BUILD_INFO_H\n\n#define BUILD_INFO \"Kinux version $VERSION (`whoami`@`hostname`) i686-elf-gcc ($GCCVERSION)\"\n\n#endif //_KINUX_BUILD_INFO_H" > include/generated/buildinfo.h
+printf "#ifndef _KINUX_BUILD_INFO_H\n#define _KINUX_BUILD_INFO_H\n\n#define BUILD_INFO \"Kinux version $VERSION (`whoami`@`hostname`) i686-elf-gcc ($GCCVERSION)\"\n\n#endif //_KINUX_BUILD_INFO_H" > include/generated/buildinfo.h
